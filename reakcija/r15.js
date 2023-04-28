3576da31fc2bbac24e7a5b976960757e65ad1b85
Annie-I <annija.iljina@gmail.com>
2023-04-28 11:08:32 +0300

    reaction second task prototype + rotation intro task prototype

--- reakcija/r15.js
+++ reakcija/r15.js
@@ -1,41 +1,61 @@
 const circles = document.querySelectorAll('.rt_task1_circle');
+let start_task_instruction = document.getElementById('instruction2');
 
 const sequence = [1, 5, 2, 3, 4, 0];
 
-const timing = [1200, 4205, 2520, 6661, 1188];
+const ms_timings = [1200, 4205, 2520, 6661, 1188];
 
-let state = 0;
-//0 - not started,
-//1 - started, 1st circle,
-//2 - 2nd circle,
-//3 - 3rd circle,
-//4 - 4th circle,
-//5 - 5th circle,
-//6 - 6th circle
+let reaction = [];
+let stimulus = [];
 
-if (state === 0) {
-    set_color(sequence[state]);
-}
+let state = 'start';
+let sequence_index = 0;
+let mistakes = 0;
+
+set_color(sequence[sequence_index]);
 
 document.addEventListener('keydown', function (event) {
     if (event.key === ' ') {
         switch (state) {
-            case 0:
-                state++;
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-                set_color(sequence[state]);
-                remove_color(sequence[state - 1]);
-                setTimeout(function () {
-                    state++;
-                }, timing[state - 1]);
+            case 'start':
+                start_task_instruction.innerHTML = 'Uzdevums sācies!';
+                start_task();
+                break;
+            case 'in_progress':
+                mistakes++;
+                console.log(mistakes);
+            case 'relocated':
+                const reaction_time = new Date().getTime();
+                reaction.push(reaction_time);
+                if (sequence_index <= 5) {
+                    start_task();
+                } else {
+                    state = 'completed';
+                    start_task_instruction.innerHTML = 'Uzdevums pabeigts! Tūlīt parādīsies nākamais uzdevums.';
+                    setTimeout(function () {
+                        //window.location.href = '../index.html';
+                    }, 3500);
+                }
+            case 'completed':
+                break;
         }
     }
 });
+
+function start_task() {
+    state = 'in_progress';
+    setTimeout(move_circle, ms_timings[sequence_index]);
+}
+
+function move_circle() {
+    remove_color(sequence[sequence_index]);
+    sequence_index++;
+    set_color(sequence[sequence_index]);
+
+    const stimulus_time = new Date().getTime();
+    stimulus.push(stimulus_time);
+    state = 'relocated';
+}
 
 function set_color(number) {
     circles[number].style.backgroundColor = 'rgb(253, 85, 73)';
@@ -45,4 +65,32 @@ function remove_color(number) {
     circles[number].removeAttribute('style');
 }
 
-// change_color(5);
+// switch (state) {
+//     case 0:
+//         state++;
+//         console.log(state);
+//     case 1:
+//         console.log(state);
+//         in_progress = 1;
+//         setTimeout(function () {
+//             set_color(sequence[state]);
+//             remove_color(sequence[state - 1]);
+//             in_progress = 0;
+//             state++;
+//         }, timing[state - 1]);
+//         if (in_progress === 1) {
+//             mistakes++;
+//             console.log('mistakes', mistakes);
+//         }
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//     // set_color(sequence[state]);
+//     // remove_color(sequence[state - 1]);
+//     // setTimeout(function () {
+//     //     state++;
+//     // }, timing[state - 1]);
+// }
+//}
---
+++ rotacija/intro.js
@@ -0,0 +1,34 @@
+let circle = document.getElementById('reaction_intro_circle');
+let start_task_instruction = document.getElementById('instruction1');
+let state = 0; //0 - not started, 1 started but color not changed, 2 color is changed, waiting reaction
+let color_change = 0;
+let mistakes = 0;
+let finish = 0;
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === ' ') {
+        if (!state) {
+            state = 1;
+            setTimeout(change_color, 5100);
+            start_task_instruction.innerHTML = 'Uzdevums sācies!';
+        } else if (state === 1) {
+            mistakes++;
+            console.log('Mistakes', mistakes);
+        } else if (state === 2 && !finish) {
+            finish = new Date().getTime();
+            console.log('finish', finish);
+            setTimeout(function () {
+                window.location.href = './r15.html';
+            }, 3500);
+            start_task_instruction.innerHTML = 'Uzdevums pabeigts! Tūlīt parādīsies nākamais uzdevums.';
+            // šeit var parātīt arī to, cik ātrs bija reakcijas laiks, ja tas tiks uzskatīts par vajadzīgu.
+        }
+    }
+});
+
+function change_color() {
+    circle.style.backgroundColor = 'rgb(14, 119, 96)';
+    state = 2;
+    color_change = new Date().getTime();
+    console.log('color changed', color_change);
+}
