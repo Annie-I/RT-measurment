2087a83b85d73d252d2671d1f8d0e3f092b26ef2
Annie-I <annija.iljina@gmail.com>
2023-05-02 21:29:33 +0300

    Survey stiling

---
+++ aptaujai.js
@@ -0,0 +1,18 @@
+const gaming_exp = document.getElementById('active_gaming_questions');
+const smoking = document.getElementById('if_smoking');
+
+function show_gaming_questions() {
+    gaming_exp.style.display = 'block';
+}
+
+function hide_gaming_questions() {
+    gaming_exp.style.display = 'none';
+}
+
+function show_smoking_question() {
+    smoking.style.display = 'block';
+}
+
+function hide_smoking_question() {
+    smoking.style.display = 'none';
+}
